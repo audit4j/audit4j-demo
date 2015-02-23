7177842bb561c31e330ae334e4dc86e84367af01
Janith Bandara <janith3000@gmail.com>
2015-02-23 19:09:11 +0530

    added initial demo project

---
+++ audit4j-demo-spring/src/main/java/org/audt4j/demo/spring/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package org.audt4j.demo.spring.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("test").password("123456").roles("USER");
+	}
+
+	//.csrf() is optional, enabled by default, if using WebSecurityConfigurerAdapter constructor
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests()
+			.antMatchers("/admin/**").access("hasRole('ROLE_USER')")
+			.and()
+				.formLogin().loginPage("/login").failureUrl("/login?error")
+					.usernameParameter("username").passwordParameter("password")
+				
+			.and()
+				.logout().logoutSuccessUrl("/login?logout")
+			.and()
+				.csrf(); 
+		
+	}
+}
---
+++ audit4j-demo-spring/src/main/java/org/audt4j/demo/spring/config/core/SpringSecurityInitializer.java
@@ -0,0 +1,7 @@
+package org.audt4j.demo.spring.config.core;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
