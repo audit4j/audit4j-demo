91228fdc2585f08d63d7312bd1b782d231d3d0ba
franck-benault <franck-benault@orange.fr>
2017-12-23 22:30:14 +0100

    complete springboot example with giving the list of roles

--- audit4j-demo-springboot/src/main/java/hello/AuditMetaData.java
+++ audit4j-demo-springboot/src/main/java/hello/AuditMetaData.java
@@ -1,9 +1,14 @@
 package hello;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.ServletRequest;
 
+import org.apache.commons.lang3.StringUtils;
 import org.audit4j.core.MetaData;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -27,9 +32,23 @@ public class AuditMetaData implements MetaData {
 	    public String getActor() {
 	        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 	        if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
-	            return ((UserDetails) authentication.getPrincipal()).getUsername();
+	            return ((UserDetails) authentication.getPrincipal()).getUsername()+ " "+getRoles();
 	        }
 	        return "anonymous";
+	    }
+	    
+	    //how to have a string giving the list of roles that the user has
+	    private String getRoles() {
+	    	
+        	List<String> roles = new ArrayList<String>();
+        	for( GrantedAuthority author :SecurityContextHolder.getContext().getAuthentication().getAuthorities()) {
+        		roles.add(author.toString());
+        	}
+        	if(roles.isEmpty()) {
+        		return "no role";
+        	} else
+        		return  "roles=["+StringUtils.join(roles, ",")+ "]";
+	    	
 	    }
 
 
